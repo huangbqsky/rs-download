3f7766999e90fca33888e21523621b0b91ac8117
huangbqsky <huangbqsky@gmail.com>
2023-04-06 10:30:29 +0800

    defalut file_path

--- src/config.rs
+++ src/config.rs
@@ -49,20 +49,27 @@ impl Config {
         let uri: Uri = matches.value_of_t("uri")?;
         let file_path = matches.value_of("file-path");
       
-        println!("passed file_path is: {:?}", file_path.unwrap_or("null"));
+        println!("you input file_path is {}", file_path.unwrap_or("null"));
 
         let file_path = match file_path {
             Some(val) => val.to_string(),
             None => {
                 // 当前目录
                 let current_dir = current_dir()?;
+                let path = Path::new(uri.path().trim());
+                let file_name = path.file_name();
                 // 文件名
-                let file_name = format!("rs_download_{:x}", md5::compute(uri.to_string()));
-                current_dir.as_path().join(file_name).to_str().unwrap().to_string()
+                let file_name = if file_name.is_none() { // 解析等 file_name 为空，则 md5 url 作为文件名
+                    format!("rs_download_{:x}", md5::compute(uri.to_string()))
+                } else {
+                    format!("rs_download_{}", file_name.unwrap().to_str().unwrap())  
+                };
+                // path = dir + file_name
+                let defalut_path = current_dir.as_path().join(file_name).to_str().unwrap().to_string();
+                println!("defalut file_path = {}", defalut_path);
+                defalut_path
             },
         };
-
-        println!("final file_path={file_path}");
 
         // 检查文件是否已存在
         if Path::new(&file_path).exists() {
